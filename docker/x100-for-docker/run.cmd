8e6081e1aff2bdd497bc3bb2e631b7c4bd817b71
tempest-observer <the.tempest.observer@gmail.com>
2026-07-04 09:50:31 +0300

    Release 20260704.0931

--- docker/x100-for-docker/run.cmd
+++ docker/x100-for-docker/run.cmd
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion 
 set container=x100-container-amd64
 
-set imageTag=tag-20260625.1712
+set imageTag=tag-20260704.0931
 set image=tempestobserver/x100-image-amd64:!imageTag!
 
 set imageLocal=x100-image-local
